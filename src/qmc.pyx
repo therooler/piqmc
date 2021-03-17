b83684e794b2db68ab481f84edb595cafc324b06
Mohamed Hibat-Allah <45025650+mhibatallah@users.noreply.github.com>
2021-03-17 17:10:57 -0400

    Add files via upload

--- src/qmc.pyx
+++ src/qmc.pyx
@@ -20,7 +20,7 @@ cpdef QuantumAnneal(np.float_t[:] sched,
                     rng):
     """
     Adapted from Hadayat Seddiqi's code see: https://github.com/hadsed/pathintegral-qmc/
-    
+
     Perform quantum annealing using path-integral quantum Monte Carlo.
 
     Args:
@@ -41,7 +41,7 @@ cpdef QuantumAnneal(np.float_t[:] sched,
                                   neighbor (second element). See
                                   tools.GenerateNeighbors().
         @rng (np.RandomState): numpy random number generator object
-        
+
     Returns:
         None: spins are flipped in-place within @svec
     """
@@ -115,6 +115,112 @@ cpdef QuantumAnneal(np.float_t[:] sched,
                         jval = nbs[sidx, s_nn, 1]
                         # Calculate the energy diff of flipping this spin
                         ediff -= 2.0 * confs[islice, sidx] * jval * confs[islice, spinidx]
+                # Metropolis accept or reject
+                if ediff >= 0.0:
+                    for trotter_i in range(slices):
+                        confs[trotter_i, sidx] *= -1
+                elif cexp(ediff / (slices * temp)) > crand()/float(RAND_MAX):
+                    for trotter_i in range(slices):
+                        confs[trotter_i, sidx] *= -1
+                ediff = 0.0
+
+#####################################################
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.embedsignature(True)
+@cython.cdivision(True)
+cpdef QuantumAnnealFullyConnected(np.float_t[:] sched,
+                    int mcsteps,
+                    int slices,
+                    float temp,
+                    int nspins,
+                    np.float_t[:, :] confs,
+                    np.float_t[:, :] couplings,
+                    rng):
+    """
+    Adapted from Hadayat Seddiqi's code see: https://github.com/hadsed/pathintegral-qmc/
+
+    Perform quantum annealing using path-integral quantum Monte Carlo.
+
+    Args:
+        @sched (np.array, float): an array of temperatures that specify
+                                  the annealing schedule
+        @mcsteps (int): number of sweeps to do on each annealing step
+        @slices (int): number of replicas
+        @temp (float): temperature after pre-annealing
+        @temp (float): ambient temperature
+        @confs (np.ndarray, float): contains the starting configurations
+                                    for all Trotter replicas
+        @couplings (np.ndarray, float): 2D array for the couplings between spins.
+        @rng (np.RandomState): numpy random number generator object
+
+    Returns:
+        None: spins are flipped in-place within @svec
+    """
+    # Define some variables
+    cdef float field = 0.0
+    cdef float jperp = 0.0
+    cdef int step = 0
+    cdef int sidx = 0
+    cdef int islice = 0
+    cdef int s_nn = 0
+    cdef float jval = 0.0
+    cdef float ediff = 0.0
+    cdef int tleft = 0
+    cdef int tright = 0
+
+    cdef np.ndarray[np.int_t, ndim=1, negative_indices=False, mode='c'] sidx_shuff = rng.permutation(range(nspins))
+
+    # Loop over temperatures
+    for field in tqdm(sched):
+        # Calculate the J_perp
+        j_perp = -1 * ((slices * temp) / 2) * clog(ctanh(field / (slices * temp)))
+
+
+        for step in range(mcsteps):
+            # Do some number of Monte Carlo sweeps
+            for islice in range(slices):
+                # Loop over spins
+                sidx_shuff = rng.permutation(range(nspins))
+                for sidx in sidx_shuff:
+                    # Loop through the given spin's neighbors
+                    for s_nn in range(nspins):
+                        jval = couplings[sidx, s_nn]
+                        # Calculate the energy diff of flipping this spin
+                        ediff -= 2.0 * confs[islice, sidx] * jval * confs[islice, s_nn]
+                    # Periodic boundary conditions
+                    if islice == 0:
+                        tleft = slices - 1
+                        tright = 1
+                    elif islice == slices - 1:
+                        tleft = slices - 2
+                        tright = 0
+                    else:
+                        tleft = islice - 1
+                        tright = islice + 1
+
+                    ediff -= 2.0 * j_perp * confs[tleft, sidx] * confs[islice, sidx]
+                    ediff -= 2.0 * j_perp * confs[islice, sidx] * confs[tright, sidx]
+
+                    # Metropolis accept or reject
+                    if ediff >= 0.0:
+                        confs[islice, sidx] *= -1
+
+                    elif cexp(ediff / (slices * temp)) > crand()/ float(RAND_MAX):
+                        confs[islice, sidx] *= -1
+
+                    ediff = 0.0
+
+            # Perform a global move
+            sidx_shuff = rng.permutation(range(nspins))
+            for sidx in sidx_shuff:
+                for islice in range(slices):
+                    # loop through all the spins
+                    for s_nn in range(nspins):
+                        jval = couplings[sidx, s_nn]
+                        # Calculate the energy diff of flipping this spin
+                        ediff -= 2.0 * confs[islice, sidx] * jval * confs[islice, s_nn]
                 # Metropolis accept or reject
                 if ediff >= 0.0:
                     for trotter_i in range(slices):
--- src/sa.pyx
+++ src/sa.pyx
@@ -9,16 +9,18 @@ from libc.stdlib cimport RAND_MAX as RAND_MAX
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-cpdef Anneal(np.float_t[:] sched, 
-             int mcsteps, 
-             np.float_t[:] svec, 
+@cython.embedsignature(True)
+@cython.cdivision(True)
+cpdef Anneal(np.float_t[:] sched,
+             int mcsteps,
+             np.float_t[:] svec,
              np.float_t[:, :, :] nbs,
              rng):
     """
     Adapted from Hadayat Seddiqi's code see: https://github.com/hadsed/pathintegral-qmc/
-    
+
     Execute thermal annealing according to @sched with @mcsteps
-    sweeps for each annealing step. Starting configuration is 
+    sweeps for each annealing step. Starting configuration is
     given by @svec, which we update in-place and calculate energies
     using the "neighbors array" @nbs.
 
@@ -36,7 +38,7 @@ cpdef Anneal(np.float_t[:] sched,
                                   neighbor (second element). See
                                   tools.GenerateNeighbors().
         @rng (np.RandomState): numpy random number generator object
-        
+
     Returns:
         None: spins are flipped in-place within @svec
     """
@@ -74,6 +76,67 @@ cpdef Anneal(np.float_t[:] sched,
                     # calculate the energy diff of flipping this spin
                     else:
                         ediff += -2.0 * svec[sidx] * (jval*svec[spinidx])
+                # Metropolis accept or reject
+                if ediff >= 0.0:  # avoid overflow
+                    svec[sidx] *= -1
+                elif cexp(ediff/temp) > crand()/float(RAND_MAX):
+                    svec[sidx] *= -1
+                # Reset energy diff value
+                ediff = 0.0
+            sidx_shuff = rng.permutation(sidx_shuff)
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.embedsignature(True)
+@cython.cdivision(True)
+cpdef AnnealFullyConnected(np.float_t[:] sched,
+             int mcsteps,
+             np.float_t[:] svec,
+             np.float_t[:, :] couplings,
+             rng):
+    """
+    Adapted from Hadayat Seddiqi's code see: https://github.com/hadsed/pathintegral-qmc/
+
+    Execute thermal annealing according to @sched with @mcsteps
+    sweeps for each annealing step. Starting configuration is
+    given by @svec, which we update in-place and calculate energies
+    using the couplings array that would correspond to a fully connected spin system
+
+    Args:
+        @sched (np.array, float): an array of temperatures that specify
+                                  the annealing schedule
+        @mcsteps (int): number of sweeps to do on each annealing step
+        @svec (np.array, float): contains the starting configuration
+        @couplings (np.ndarray, float): 2D array of couplings between spins.
+        @rng (np.RandomState): numpy random number generator object
+
+    Returns:
+        None: spins are flipped in-place within @svec
+    """
+    # Define some variables
+    cdef int nspins = svec.size
+    cdef int schedsize = sched.size
+    cdef int itemp = 0
+    cdef float temp = 0.0
+    cdef int step = 0
+    cdef int sidx = 0
+    cdef int s_nn = 0
+    cdef float jval = 0.0
+    cdef float ediff = 0.0
+    cdef np.ndarray[np.int_t, ndim=1] sidx_shuff = rng.permutation(range(nspins))
+
+    # Loop over temperatures
+    for itemp in tqdm(range(sched.size)):
+        # Get temperature
+        temp = sched[itemp]
+        # Do some number of Monte Carlo steps
+        for step in range(mcsteps):
+            # Loop over spins
+            for sidx in sidx_shuff:
+                # loop through all the spins
+                for s_nn in range(nspins):
+                    jval = couplings[sidx,s_nn]
+                    ediff += -2.0 * svec[sidx] * (jval*svec[s_nn])
                 # Metropolis accept or reject
                 if ediff >= 0.0:  # avoid overflow
                     svec[sidx] *= -1
